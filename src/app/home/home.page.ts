ca0131b9009c75310e807f4f59102d4e29d83e90
Antonio Montalva <tonimontalvapastor@gmail.com>
2019-12-09 13:39:38 +0100

    Add appflow sdk. AE7

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  hogar : boolean = false;
+  motor: boolean = false;
+  inmobiliaria : boolean = false;
+  tecnologia : boolean = false;
+  categoria_Seleccionada : boolean = false;
+
   constructor() {}
 
+  categoria_Hogar() : void {
+    if(this.inmobiliaria == true || this.motor == true || this.tecnologia == true){
+      this.inmobiliaria = false;
+      this.motor = false;
+      this.tecnologia = false;
+    }
+    this.hogar = true;
+    this.categoria_Seleccionada = true;
+  }
+
+  categoria_Motor() : void {
+    if(this.inmobiliaria == true || this.hogar == true || this.tecnologia == true){
+      this.inmobiliaria = false;
+      this.hogar = false;
+      this.tecnologia = false;
+    }
+    this.motor = true;
+    this.categoria_Seleccionada = true;
+  }
+
+  categoria_Inmobiliaria() : void {
+    if(this.motor == true || this.hogar == true || this.tecnologia == true){
+      this.motor = false;
+      this.hogar = false;
+      this.tecnologia = false;
+    }
+    this.inmobiliaria = true;
+    this.categoria_Seleccionada = true;
+  }
+
+  categoria_Tecnologia() : void {
+    if(this.motor == true || this.hogar == true || this.inmobiliaria == true){
+      this.motor = false;
+      this.hogar = false;
+      this.inmobiliaria = false;
+    }
+    this.tecnologia = true;
+    this.categoria_Seleccionada = true;
+  }
 }
